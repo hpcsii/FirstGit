0fd7bfa52b2c735dc4a2c17c8c9560a8d6e5f264
hpcsii <13071298568@163.com>
2018-08-03 10:03:51 +0800

    Initial commit

---
+++ src/com/csii/hp/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * ModifiedBy:   hepeng
+ * Date:     2018/8/3 上午10:01
+ */
+package com.csii.hp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
